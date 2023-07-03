a148bb82e59754141eb0d91f49907317847f32d8
VictorEJM <vjaumemorado@gmail.com>
2023-07-04 00:47:56 +0200

    Implement Item and player status management, apply Item script to item prefabs and replace the prefab name: ItemShell to ItemAmmo

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -10,12 +10,43 @@ public class TankHealth : MonoBehaviour
     public Color m_ZeroHealthColor = Color.red;         // The color the health bar will be when on no health.
     public GameObject m_ExplosionPrefab;                // A prefab that will be instantiated in Awake, then used whenever the tank dies.
 
-
     private AudioSource m_ExplosionAudio;               // The audio source to play when the tank explodes.
     private ParticleSystem m_ExplosionParticles;        // The particle system the will play when the tank is destroyed.
     private float m_CurrentHealth;                      // How much health the tank currently has.
     private bool m_Dead;                                // Has the tank been reduced beyond zero health yet?
 
+    // shield
+    [HideInInspector] public bool hasShield;
+
+    // light to recognize if the tank has shield
+    [HideInInspector] public GameObject lightGameObject;
+    [HideInInspector] public Light lightComp;
+
+    // Activate shield and set the light
+    public void ActivateShield()
+    {
+        if (!hasShield) // if it was already activated, don't set the light again
+        {
+            lightGameObject = new GameObject("The Light");
+            lightComp = lightGameObject.AddComponent<Light>();
+            lightComp.color = Color.green;
+            lightComp.intensity = 4.2f;
+            lightGameObject.transform.position = // set the position near to the tank
+                            new Vector3(this.transform.position.x, this.transform.position.y + 3, this.transform.position.z);
+            lightGameObject.transform.SetParent(this.transform); // set the parent position to the tank
+        }
+        hasShield = true;
+    }
+
+    public void DisableShield()
+    {
+        // tank shield disabled by default
+        hasShield = false;
+
+        // remove the shield light when starting
+        Destroy(lightGameObject);
+        Destroy(lightComp);
+    }
 
     private void Awake()
     {
@@ -29,6 +60,10 @@ public class TankHealth : MonoBehaviour
         m_ExplosionParticles.gameObject.SetActive(false);
     }
 
+    private void Start()
+    {
+        DisableShield();
+    }
 
     private void OnEnable()
     {
@@ -43,6 +78,16 @@ public class TankHealth : MonoBehaviour
 
     public void TakeDamage(float amount)
     {
+        // Don't destroy/receive damage if shield is activated
+        if (hasShield)
+        {
+            // remove the light
+            Destroy(lightGameObject);
+            Destroy(lightComp);
+            hasShield = false;
+            return;
+        }
+
         // Reduce current health by the amount of damage done.
         m_CurrentHealth -= amount;
 
