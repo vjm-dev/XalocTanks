14df65a07526547db98f2ea619bea6a6b998ff64
vjm-dev <vjaume@outlook.com>
2023-07-04 22:16:10 +0200

    Change the explosion radius to a tiny value, reason: a single projectile hit received will destroy the tank

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -7,7 +7,7 @@ public class ShellExplosion : MonoBehaviour
     public AudioSource m_ExplosionAudio;                // Reference to the audio that will play on explosion.
     public float m_ExplosionForce = 1000f;              // The amount of force added to a tank at the centre of the explosion.
     public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
-    public float m_ExplosionRadius = 5f;                // The maximum distance away from the explosion tanks can be and are still affected.
+    public float m_ExplosionRadius = 0.5f;              // The maximum distance away from the explosion tanks can be and are still affected.
 
 
     private void Start()
